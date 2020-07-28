3eda0354149982ac60b01117464fa5b2b18b64e3
paulwilliams <paalpwmd>
2020-07-27 20:09:24 -0500

    update assignment

--- script.js
+++ script.js
@@ -25,6 +25,21 @@ window.onload = (event) => {
   let faultyItems = document.getElementById('faultyItems');
   let launchStatus = document.getElementById('launchStatus');
 
+  fetch('https://handlers.education.launchcode.org/static/planets.json')
+    .then((response) => response.json())
+    .then((data) => {
+      let ranNum = Math.floor(Math.random() * data.length);
+      missionTarget.innerHTML = `<h2>Mission Destination</h2>
+          <ol>
+            <li>Name: ${data[ranNum].name}</li>
+            <li>Diameter: ${data[ranNum].diameter}</li>
+            <li>Star: ${data[ranNum].star}</li>
+            <li>Distance from Earth: ${data[ranNum].distance}</li>
+            <li>Number of Moons: ${data[ranNum].moons}</li>
+          </ol>
+          <img src="${data[ranNum].image}">`;
+    });
+
   document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
     if (pilotName.value == '') {
@@ -37,6 +52,8 @@ window.onload = (event) => {
       alert('Please enter a number value for fuel level');
     } else if (cargoMass.value == '') {
       alert('Please enter a cargo mass.');
+    } else if (isNaN(fuelLevel.value)) {
+      alert('Please enter a number value for cargo mass');
     } else {
       pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch`;
       copilotStatus.innerHTML = `Co-pilot ${coPilotName.value} is ready for launch`;
@@ -56,21 +73,13 @@ window.onload = (event) => {
       if (Number(cargoMass.value < 1000) && Number(fuelLevel.value) > 10000) {
         launchStatus.style.color = 'green';
         launchStatus.innerHTML = 'Shuttle ready for launch';
+        faultyItems.style.visibility = 'visible';
+        cargoStatus.innerHTML = `Cargo mass low enough for launch.`;
+        fuelStatus.innerHTML = `Fuel level high enough for launch`;
       }
-      fetch('https://handlers.education.launchcode.org/static/planets.json')
-        .then((response) => response.json())
-        .then((data) => {
-          let ranNum = Math.floor(Math.random() * data.length);
-          missionTarget.innerHTML = `<h2>Mission Destination</h2>
-          <ol>
-            <li>Name: ${data[ranNum].name}</li>
-            <li>Diameter: ${data[ranNum].diameter}</li>
-            <li>Star: ${data[ranNum].star}</li>
-            <li>Distance from Earth: ${data[ranNum].distance}</li>
-            <li>Number of Moons: ${data[ranNum].moons}</li>
-          </ol>
-          <img src="${data[ranNum].image}">`;
-        });
     }
   });
 };
+
+// Your info box does not populate with the information on the shuttle if everything passes
+// The fetch does not populate until the submit button is hit
